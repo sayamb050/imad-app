00266392053edc93d83bf69583d50c6a2a460015
sayamb050 <30860222+sayamb050@users.noreply.github.com>
2017-08-15 21:07:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,8 +97,9 @@ return htmlTemplate
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/:articleName', function (req, res) {
-var articleName = req.params.articleName;
+  var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
