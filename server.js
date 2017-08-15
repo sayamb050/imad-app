5837070b7618d297457726ddfdef618eb5dcfe41
sayamb050 <30860222+sayamb050@users.noreply.github.com>
2017-08-15 19:53:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/article-one', function (req, res) {
    res.send(createHtmlTemplate(article-one));
 });
 app.get('/article-two', function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+     res.send(createHtmlTemplate(article-two));
 });
 app.get('/article-three', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
