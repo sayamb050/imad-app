cc2331aff0439de96b772e44b336bb3ac00299e5
sayamb050 <30860222+sayamb050@users.noreply.github.com>
2017-08-15 17:19:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-    res.sendFile('article one requested will be served here');
+    res.send('article one requested will be served here');
 });
 app.get('/article-two', function (req, res) {
-    res.sendFile('article two requested will be served here');
+    res.send('article two requested will be served here');
 });
 app.get('/article-three', function (req, res) {
-    res.sendFile('article three requested will be served here');
+    res.send('article three requested will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
